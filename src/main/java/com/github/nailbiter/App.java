ed248fca4c67c9de86947d5a01d27e45c74d4421
Alex Leontiev <alozz1991@gmall.com>
2018-08-30 11:38:15 +0900

    initial commit

---
+++ src/main/java/com/github/nailbiter/App.java
@@ -0,0 +1,13 @@
+package com.github.nailbiter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
